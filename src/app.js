d6621cbdcc4b040e27644dd05d259d5712c56c5c
April <thethtaryeem@gmail.com>
2023-06-08 02:33:43 +0630

    Added event listeners

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
-let cityCurrent = "Burma";
-
 function formatDate(timestamp) {
   let date = new Date();
   let hours = date.getHours();
-  if (hours < 10) return `0${hours}`;
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
 
   let minutes = date.getMinutes();
-  if (minutes < 10) return `0${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
   let days = [
     "Sunday",
@@ -22,11 +24,10 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
-  console.log(response.data.country);
-
   let cityElement = document.querySelector("#CurrentCity");
   cityElement.innerHTML = response.data.city;
+
+  celsiusTemp = response.data.temperature.current;
 
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -51,8 +52,42 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "29baaftfaf333ad6ca3704ob80d346c8";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityCurrent}&key=${apiKey}&units=metric`;
-console.log(apiUrl);
+function search(city) {
+  let apiKey = "29baaftfaf333ad6ca3704ob80d346c8";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+function showCelsius(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
+}
+
+function showFahrenheit(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round((celsiusTemp * 9) / 5 + 32);
+}
+
+let celsiusTemp = null;
+
+let celsiusLink = document.querySelector("#cel-link");
+celsiusLink.addEventListener("click", showCelsius);
+
+let fahrenheitLink = document.querySelector("#fah-link");
+fahrenheitLink.addEventListener("click", showFahrenheit);
